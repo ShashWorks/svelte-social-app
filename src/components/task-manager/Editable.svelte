247e01bc0f701dd9adba97e8b17106940fadd511
ShashWorks <shashwat.gupta@michelin.com>
2024-08-02 22:25:06 +0530

    value binding and events

---
+++ src/components/task-manager/Editable.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  export let value;
+  let isEditing = false;
+</script>
+
+{#if isEditing}
+  <div class="flex-it">
+    <!-- <textarea bind:value={value} /> -->
+    <textarea bind:value />
+    <button
+      on:click={() => {
+        isEditing = false;
+        dispatch("editClose", {
+          taskText: value
+        });
+        //basically we can pass anything by creating an object
+        // dispatch("editClose", {
+        //   taskText: "Hi There!",
+        //   someNUm: 100 
+        // });
+      }}
+      class="flex underline">Close</button
+    >
+  </div>
+{:else}
+  <button
+    on:click={() => {
+      isEditing = true;
+    }}
+  >
+    <slot />
+  </button>
+{/if}
